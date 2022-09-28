8bfe33a487730298789ad4a7f373265cca922337
Victor Rivas <victor_rivas@epam.com>
2022-09-28 16:27:48 -0400

    UI for search

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import MusicPlayer from "./components/MusicPlayer";
+import { Searchbar } from "./components/Searcbar";
 import { Sidebar } from "./components/Sidebar";
 import { TopPlay } from "./components/TopPlay";
 import { AroundYou } from "./pages/AroundYou";
 import { ArtistDetails } from "./pages/ArtistDetails";
-import Discover from "./pages/Discover"
+import Discover from "./pages/Discover";
+import { Search } from "./pages/Search";
 import { SongDetails } from "./pages/SongDetails";
 import { TopArtists } from "./pages/TopArtists";
 import { TopCharts } from "./pages/TopCharts";
-import { useAppSelector } from "./redux/hooks"
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
   const { activeSong } = useAppSelector((store) => store.player);
@@ -17,16 +19,17 @@ function App() {
     <div className="relative flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <Searchbar />
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
-              <Route path='/' element={<Discover />} />
-              <Route path='/songs/:songid' element={<SongDetails />} />
-              <Route path='/artists/:id' element={<ArtistDetails />} />
-              <Route path='/around-you' element={<AroundYou />} />
-              <Route path='/top-charts' element={<TopCharts />} />
-              <Route path='/top-artists' element={<TopArtists />} />
-              {/* <Searchbar />*/}
+              <Route path="/" element={<Discover />} />
+              <Route path="/songs/:songid" element={<SongDetails />} />
+              <Route path="/artists/:id" element={<ArtistDetails />} />
+              <Route path="/around-you" element={<AroundYou />} />
+              <Route path="/top-charts" element={<TopCharts />} />
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/search/:searchterm" element={<Search />} />
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 hfit">
@@ -40,7 +43,7 @@ function App() {
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
